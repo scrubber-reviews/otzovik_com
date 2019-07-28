17a07916c64e4fc1071129e99d58f7e4d0fb6873
Melis <melis.zhoroev@gmail.com>
2019-07-29 06:43:11 +0700

    friendly import provider

--- otzovik_com/__init__.py
+++ otzovik_com/__init__.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for Otzovik com."""
+from .otzovik_com import OtzovikCom
 
 __author__ = """NMelis"""
 __email__ = 'melis.zhoroev@gmail.com'
 __version__ = '0.1.0'
+
+
+provider = OtzovikCom
