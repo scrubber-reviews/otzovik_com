5697f558404a8a235468bb600e1e34a8bf35dc84
Melis <melis.zhoroev@gmail.com>
2019-08-12 19:07:51 +0700

    added rating class to __init__.py

--- otzovik_com/__init__.py
+++ otzovik_com/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for Otzovik com."""
-from .otzovik_com import OtzovikCom
+from .otzovik_com import OtzovikCom, Rating
 
 __author__ = """NMelis"""
 __email__ = 'melis.zhoroev@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __name__ = 'Отзовик'
 __slug_img_link__ = 'https://i.ibb.co/jfyL3hK/image.png'
 __how_get_slug__ = """
@@ -15,3 +15,4 @@ Slug это https://otzovik.com/reviews/ТО_ЧТО_ЗДЕСЬ/ (без слеш
 
 
 provider = OtzovikCom
+rating = Rating
